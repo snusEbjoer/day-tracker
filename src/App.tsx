8de53a2a2e2ace069a6db4a8be745ce2363cf002
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-04 22:09:58 +0300

    refactor(App): remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { ReactElement, ReactNode, useState } from "react";
 import "./App.css";
 import { CheckList } from "./components/CheckList/CheckList";
 import { Layout } from "./components/Layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import { Kanban } from "./components/Kanban/Kanban";
 function App() {
-  const [count, setCount] = useState(0);
   const LayoutWrapper = (element: JSX.Element) => {
     return <Layout>{element}</Layout>;
   };
