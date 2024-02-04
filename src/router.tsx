afe99b3f7169412a4cc1be873d7d554dcf0754bc
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-04 23:26:11 +0300

    feat: add new index route

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { CheckList } from "./pages/CheckList";
 import { Kanban } from "./pages/Kanban";
+import { DashboardPage } from "./components/DashboardPage/DashboardPage";
 
 export const router = createBrowserRouter([
     {
         path: '/', element: <Layout />,
         children: [
-            { index: true, element: <CheckList /> },
+            { index: true, element: <DashboardPage /> },
+            { path: 'checklist', element: <CheckList /> },
             { path: 'kanban', element: <Kanban /> }
         ],
     }
