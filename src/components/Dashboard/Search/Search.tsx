edd3583c909646539c49363edc9cd2ca823d1ae8
Nikita Tyrkin <tyrkinn@pm.me>
2024-02-04 23:27:01 +0300

    refactor: reformat

--- src/components/Dashboard/Search/Search.tsx
+++ src/components/Dashboard/Search/Search.tsx
@@ -2,10 +2,10 @@ import './Search.css';
 
 export const Search = () => {
 
-				return (
-								<div className="dashboard_search">
-												<input placeholder='Search...' className="search_input"/>
-								</div>
-				)
+	return (
+		<div className="dashboard_search">
+			<input placeholder='Search...' className="search_input" />
+		</div>
+	)
 
 }
